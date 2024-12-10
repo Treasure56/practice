913fd6465277c8e1f19e2cc5137ea9e7404f50e4
treasure56 <simplicitytreasure@gmail.com>
2024-12-10 12:03:50 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,207 @@
+// // for(let i = 20; i >= 10; i--){
+// //     console.log(i)
+// // }
+
+// // for(let i = 100; i >= 10; i-=10){
+// //     console.log(i)
+// // }
+
+// // 100
+// // 10
+// // 100 90 80
+
+// for(let i = 1; i <= 20; i+=2){
+//     console.log(i)
+// }
+
+// // for(let i = 0; i <= 30; i+=2){
+// //     console.log(i)
+// // }
+// // for(let i = 1; i <= 1000; i+=i){
+// //     console.log(i)
+// // }
+
+// // for(let i = 1; i <= 12; i++){
+// //     console.log(i*12)
+// // }
+// // 1*12 2*12 ... 12
+
+
+// // for (let i = 0; i <= 10; i += 2) {
+// //     console.log(i);
+// //   }
+
+//   console.log("\n\n")
+// //   to print odd numbers
+
+// // for(let i = 1; i <= 10; i+=2){
+// //     console.log(i)
+// // }
+
+// //  to print multiplication table
+
+// // for(let i = 0; i <= 12; i++){
+// //     console.log(`2 x ${i} = ${2 * i}`)
+// // }
+
+// // to check if a number is divisible by 2
+
+// // for(let i = 1; i <= 60; i++ ){
+// //     if(i % 2 === 0){
+// //         console.log(i)
+// //     }
+// // }
+
+// // counting letters in a string
+// // const word = "hello"
+
+// // for(let i = 0; i < word.length; i++){
+// //     console.log(word[i])
+// // }
+
+
+// // using loop to sum numbers
+// // let  sum = 0;
+
+// // for(let i = 1; i <= 5; i++ ){
+// //     sum += i;
+// // }
+// // console.log("Total sum: ", sum);
+
+
+// // loop with break
+// //  for(let i = 1; i <= 10; i++){
+// //     if(i === 7){
+// //         break;
+// //     }
+// //     console.log(i);
+    
+// //  }
+
+//  // loop with continue
+
+// //  for(let i = 1; i <= 10; i++){
+// //     if(i === 7){
+// //         continue;
+// //     }
+// //     console.log(i);
+    
+// //  }
+
+// for(let i = 1; i <= 1; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+
+// console.log("\n -----------------------------------------------------\n")
+
+
+// for(let i = 2; i <= 2; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 3; i <= 3; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 4; i <= 4; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 5; i <= 5; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 6; i <= 6; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 7; i <= 7; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 8; i <= 8; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 9; i <= 9; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 10; i <= 10; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+// for(let i = 11; i <= 11; i++){
+//     for(let c = 1; c <= 12; c++){
+//         console.log(`${i} x ${c} = ${i * c}`);
+//     }
+// }
+// console.log("\n -----------------------------------------------------\n")
+
+
+// for(let i = 1; i < 12; i++){
+//     for(let j = 1; j < 12; j++){
+//         console.log(`${i} x ${j} = ${i * j}`)
+//     }
+
+//     console.log("-----------------------------------")
+// }
+
+
+// to print even numbers
+
+// for(let i = 0; i <= 20; i+=2){
+//     console.log(i)
+// }
+
+
+  
+  
+
+
+
+
+
+
+// // to loop through array
+
+// // const colors = ["red", "blue", "green", "yellow"]
+
+// // for( let i = 0; i < colors.length; i++){
+// //     console.log(colors)
+// // }
+
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i, "is even");
+}  
+  
